e7f3133204cf54f66ed3fed326457e38d26d2a40
Carlos Roberto <carlosmedeiroslima@gmail.com>
2023-02-11 18:25:16 -0300

    Atualizando os microserviços

---
+++ microserviços/ms-fraude/src/test/java/br/com/igti/posgraduacao/msfraude/MsFraudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igti.posgraduacao.msfraude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFraudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microserviços/ms-prospecto/src/main/java/br/com/igti/posgraduacao/msprospecto/MsProspectoApplication.java
@@ -0,0 +1,13 @@
+package br.com.igti.posgraduacao.msprospecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProspectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProspectoApplication.class, args);
+	}
+
+}
